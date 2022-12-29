f334b1dca077dbeb22d7e732a3c1dcd76bdde97f
Vorpholomey <117433309+Vorpholomey@users.noreply.github.com>
2022-12-29 15:52:00 +0700

    Add files via upload
    Добавлен файл locators.py где описываем все локаторы по которым осуществляем поиск
    Добавлен файл login_page.py где описываем проверки корректности url, формы логина, формы регистрации на странице логина
    Добавлен в файл main_page.py в go_to_login_page алерты 
    Добавлены в файл test_main_page.py тест test_register_login_form_and_link проверки корректности url, формы логина, формы регистрации на странице логина

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,4 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from .base_page import BasePage
+from .locators import MainPageLocators
+from .locators import LoginPageLocators
+from .login_page import LoginPage
 
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        login_link.click()
+        return LoginPage(browser=self.browser, url=self.browser.current_url)
+        alert = self.browser.switch_to.alert
+        alert.accept() 
 
+    def should_be_login_link(self):
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+    
+#    def go_to_login_page(self):
+#        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+#        link.click()
+
+    
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,32 @@
+#import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
+from .pages.locators import LoginPageLocators
+from .pages.login_page import LoginPage
+
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    page.should_be_login_link()      # проверяем, что есть ссылка, которая ведет на логин 
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
+
+
+def test_register_login_form_and_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+    page.should_be_register_form()
+    page.should_be_login_url()
+
+
+
+#    page.should_be_login_link()      # проверяем, что есть ссылка, которая ведет на логин 
+#    page.should_be_login_link()      # проверяем, что есть ссылка, которая ведет на логин 
+#    page.should_be_login_link()      # проверяем, что есть ссылка, которая ведет на логин 
+#    page.should_be_login_link()      # проверяем, что есть ссылка, которая ведет на логин 
